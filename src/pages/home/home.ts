76d007bd47a83a46a0ce30975a743324da1fa71f
Jorge Vergara <javebratt@gmail.com>
2018-06-11 15:52:37 -0500

    Adds the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the HomePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
+import { FirebaseProvider } from '../../providers/firebase/firebase';
+import { Observable } from 'rxjs';
 
 @IonicPage({
   name: 'home'
@@ -16,12 +11,58 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  public articles: Observable<any>;
+  public userId: string = null;
+  public isUser: boolean = false;
+  constructor(
+    public navCtrl: NavController,
+    public firebaseProvider: FirebaseProvider,
+    public alertCtrl: AlertController
+  ) { }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad HomePage');
+    this.articles = this.firebaseProvider.getLatestNews().valueChanges();
+    this.firebaseProvider.getUser().subscribe(user => {
+      if (user) {
+        this.userId = user.uid;
+        this.isUser = true;
+      }
+    });
+  }
+
+  articleDetail(articleId: string): void {
+    this.navCtrl.push('article-detail', {
+      id: articleId
+    });
+  }
+
+  bookmarkArticle(article: any): void {
+    if (this.isUser) {
+      this.firebaseProvider.bookmarkArticle(article);
+    } else {
+      const alert = this.alertCtrl.create({
+        title: 'You need an account',
+        message: 'In order to bookmark a story you need to login with Facebook',
+        buttons: [
+          {
+            text: 'No way!',
+            handler: () => {
+              console.log("Didn't want to login");
+            }
+          },
+          {
+            text: 'Ok',
+            handler: () => {
+              this.firebaseProvider.signInUser().then(() => {
+                this.bookmarkArticle(article);
+                alert.dismiss();
+              });
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
   }
 
 }
--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,17 +1,63 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  AngularFireDatabase,
+  AngularFireList,
+  AngularFireObject
+} from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase/app';
+import { Observable } from 'rxjs';
 
-/*
-  Generated class for the FirebaseProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class FirebaseProvider {
+  public userId: string;
 
-  constructor(public http: HttpClient) {
-    console.log('Hello FirebaseProvider Provider');
+  constructor(
+    public afAuth: AngularFireAuth,
+    public afDatabase: AngularFireDatabase
+  ) {
+    afAuth.authState.subscribe(user => {
+      if (user) {
+        this.userId = user.uid;
+      }
+    });
+  }
+
+  getLatestNews(): AngularFireList<any> {
+    return this.afDatabase.list('articles');
+  }
+
+  getUser(): Observable<any> {
+    return this.afAuth.authState;
+  }
+
+  signInUser(): Promise<any> {
+    return this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(user => {
+        this.afDatabase.object(`userProfile/${user.user.uid}`).update({
+          displayName: user.user.displayName,
+          email: user.user.email,
+          photoURL: user.user.photoURL
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  bookmarkArticle(article: any): Promise<any> {
+    return this.afDatabase
+      .object(`articles/${article.id}/bookmarked/${this.userId}`)
+      .set(true)
+      .then(() => {
+        this.afDatabase
+          .object(`userProfile/${this.userId}/articles/${article.id}`)
+          .set({
+            description: article.description,
+            title: article.title,
+            urlToImage: article.urlToImage,
+            id: article.id
+          });
+      });
   }
 
 }
